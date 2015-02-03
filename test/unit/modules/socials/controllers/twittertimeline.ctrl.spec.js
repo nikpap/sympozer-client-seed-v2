23e1e62dba03891efddeac719a08fd7c30f46afd
Rachid Delory <rachid.delory@gmail.com>
2015-02-03 12:04:34 +0100

    Ajout test twittertimeline

---
+++ test/unit/modules/socials/controllers/twittertimeline.ctrl.spec.js
@@ -0,0 +1,28 @@
+/**
+ * @TODO : define tests
+ */
+describe('Test twitterTimelineCtrl - ', function() {
+
+    //Sympozer app has to be loaded first
+    beforeEach(module('sympozerApp', 'socialsApp'));
+
+    var ctrl, scope;
+
+    //Loading of the  categoriesDeleteCtrl
+    beforeEach(inject(function($controller, $rootScope) {
+        // Create a new scope that's a child of the $rootScope
+        scope = $rootScope.$new();
+        // Create the controller
+        ctrl = $controller('twitterTimelineCtrl', {
+            $scope: scope,
+            categoryModel : { id: 1 }
+        });
+    }));
+
+    //Verify that scope values are well formed
+    it('should load scope values', function() {
+        expect($scope.tweets).toBeDefined();
+        expect($scope.person).toContain("twitterid");
+    });
+
+});
